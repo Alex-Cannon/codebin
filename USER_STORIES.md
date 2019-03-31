7d60970bf2a789fa133136d98f5ad741d7d2bb86
Alex <alexcannon2018@gmail.com>
2019-03-31 16:42:18 -0600

    chore: basic setup done

---
+++ USER_STORIES.md
@@ -0,0 +1,13 @@
+# User Stories
+
+#### User
+* I can signup/login/logout with my email, Google, or Github.
+* I can CRUD my account
+
+#### Codebin
+* I can CRUD a Codebin live.
+* I can edit anyone's Codebin live in my browser. This will NOT change a given Codebin.
+
+#### Search
+* I can text search for Codebins 
+
---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-10">
+            <h1>CodeBin</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
