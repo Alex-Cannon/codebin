80e2559ba35435b426faf042fa67b3a2084231ec
Alex <alexcannon2018@gmail.com>
2019-04-23 17:17:56 -0600

    added /build to repo

---
+++ build/precache-manifest.35926b7d82a0e079786aaaba08fe8b07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bb50222e9f92cb114624",
+    "url": "/static/js/main.2836c28b.chunk.js"
+  },
+  {
+    "revision": "5c5bd7997f94ea9f2750",
+    "url": "/static/js/2.3dccba8f.chunk.js"
+  },
+  {
+    "revision": "bb50222e9f92cb114624",
+    "url": "/static/css/main.fefd2ed8.chunk.css"
+  },
+  {
+    "revision": "2b21acadaa931992342d4f9da0452904",
+    "url": "/index.html"
+  }
+];
