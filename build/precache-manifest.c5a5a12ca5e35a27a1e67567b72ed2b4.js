b90ecdfc9554ad7f3769c73c29b8fc78256978da
Alex <alexcannon2018@gmail.com>
2019-04-24 16:55:45 -0600

    not ignore /build

---
+++ build/precache-manifest.c5a5a12ca5e35a27a1e67567b72ed2b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a09f5dc7b5f8f9bc8e",
+    "url": "/static/js/main.c0929779.chunk.js"
+  },
+  {
+    "revision": "bca0e307cb5be999446c",
+    "url": "/static/js/2.3c5a75ef.chunk.js"
+  },
+  {
+    "revision": "12a09f5dc7b5f8f9bc8e",
+    "url": "/static/css/main.09ae58d4.chunk.css"
+  },
+  {
+    "revision": "80294743f09e429a1736a94eb3b36609",
+    "url": "/index.html"
+  }
+];
